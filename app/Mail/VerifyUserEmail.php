7617c64b9a3acd30ba0f52716f01250ed6dc1c4a
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-07-17 19:14:29 +0100

    new commit

--- app/Mail/VerifyUserEmail.php
+++ app/Mail/VerifyUserEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class VerifyUserEmail extends Mailable implements ShouldQueue
+class VerifyUserEmail extends Mailable 
 {
     use Queueable, SerializesModels;
  
