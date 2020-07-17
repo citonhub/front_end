abb587903447c062b2eb656a1d8cb075e6c8d74a
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-07-17 17:22:39 +0100

    new push

--- resources/js/components/home/HomeController.vue
+++ resources/js/components/home/HomeController.vue
@@ -24,7 +24,7 @@
       :data-key="'id'"
       :data-sources="post_data"
       :data-component="itemComponent"
-      :keeps="15"
+      :keeps="30"
 
 :style="'background:white; font-family:BodyText;position:absolute;left:0; width:100%; height:100%; overflow-y:' + this.$root.scrollHome + '; overflow-x:hidden; padding-top:8px;padding-bottom:120px;'">
         
