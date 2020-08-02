e453b2ad3fb75f81682a030b77e49a42f38829e4
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-08-02 22:21:45 +0100

    new changes

--- resources/js/components/home/NewPost.vue
+++ resources/js/components/home/NewPost.vue
@@ -322,6 +322,8 @@ export default {
       var urlRegex =  /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
      return text.replace(urlRegex, function(url) {
      return '<a href="' + url + '" target="_blank">' + url + '</a>';
+
+      this.UrlValue = url;
   })
    // or alternatively
     // return text.replace(urlRegex, '<a href="$1">$1</a>')
@@ -412,6 +414,7 @@ export default {
          }
          
 
+     
           this.contentInWord = this.urlify(this.editor.getHTML());
 
           
