4464a787f6a635c991a2f8eeb4933f6734158c53
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-08-02 10:42:26 +0100

    image viewer

--- resources/js/components/home/FullImageViewer.vue
+++ resources/js/components/home/FullImageViewer.vue
@@ -9,7 +9,7 @@
 
       
 
-       <div class="col-12 py-0 my-0 px-0 d-flex" style="position:absolute; height:90%;width:100%; overflow-y:auto; overflow-x:hidden; align-items:center; justify-content:center;">
+       <div class="col-12 py-0 my-0 px-0 d-flex" style="position:absolute; height:80%; top:100%;width:100%; overflow-y:auto; overflow-x:hidden; align-items:center; justify-content:center;">
           <v-btn icon color="#ffffff" @click="goBack" style="position:absolute;background:#3E8893;top:3%; left:3%;" 
            class="d-inline-block  "><v-icon>mdi-close mdi-18px</v-icon></v-btn>
 
