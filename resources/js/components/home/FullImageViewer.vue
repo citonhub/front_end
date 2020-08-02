cf7835bd966feab279d3d67fee9e116c5113754c
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-08-02 11:15:46 +0100

    image viewer

--- resources/js/components/home/FullImageViewer.vue
+++ resources/js/components/home/FullImageViewer.vue
@@ -7,11 +7,12 @@
          <div :style="'position:absolute; height:100%; width:100%; overflow-y:auto;left:0;top:0%;background:' + this.$root.fullImageColor +';'"> 
 
 
-      
+          <v-btn icon color="#ffffff" @click="goBack" style="position:absolute;background:#3E8893;top:4%; left:3%; z-index:990679797879;" 
+           class="d-inline-block  "><v-icon>mdi-close mdi-18px</v-icon></v-btn>
+
 
        <div class="col-12 py-0 my-0 px-0 d-flex" style="position:absolute; height:80%; top:10%; width:100%; overflow-y:auto; overflow-x:hidden; align-items:center; justify-content:center;">
-          <v-btn icon color="#ffffff" @click="goBack" style="position:absolute;background:#3E8893;top:3%; left:3%; z-index:990679797879;" 
-           class="d-inline-block  "><v-icon>mdi-close mdi-18px</v-icon></v-btn>
+        
 
            <v-img :src="this.$root.imageViewPath"></v-img>
        </div>
