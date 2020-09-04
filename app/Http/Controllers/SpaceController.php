ff9659e9f7bc89db36a5b4c7f05646818ce305fd
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-09-04 11:29:21 +0100

    video upload fix

--- app/Http/Controllers/SpaceController.php
+++ app/Http/Controllers/SpaceController.php
@@ -71,7 +71,7 @@ class SpaceController extends Controller
 
       $r_240p  = (new Representation)->setKiloBitrate(150)->setResize(426, 240);
       $r_360p  = (new Representation)->setKiloBitrate(276)->setResize(640, 360);
-      $r_720p  = (new Representation)->setKiloBitrate(2048)->setResize(1280, 720);
+     
      
     
    
@@ -79,7 +79,7 @@ class SpaceController extends Controller
       ->setSegDuration(10) // Default value is 10 
       ->setAdaption('id=0,streams=v id=1,streams=a')
       ->x264()
-      ->addRepresentations([$r_240p, $r_360p, $r_720p])
+      ->addRepresentations([$r_240p, $r_360p])
       ->save('/var/www/citonhubnew/public/videos/'.  $videoName .'.mpd');
 
 
