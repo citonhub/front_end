faf173ff58a63b3ac15ab7a087ebe08fb9e46077
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-09-13 13:08:25 +0100

    main video player changes

--- resources/js/components/space/MainVideo.vue
+++ resources/js/components/space/MainVideo.vue
@@ -3,7 +3,7 @@
 
    <div>
      <div  class="d-flex alignContent col-12 py-0 my-0"  v-if="!playVideoValue"
-   :style="'border:1px solid transparent;width:100%;height:200px; background-color:'+ backgroundColor+ ';background-image:url('+ backgroundImg+ '); border-radius: 10px;'" :id="'playerContainer'+playerId">
+   :style="'border:1px solid transparent;width:100%;height:200px; background-size:cover; background-repeat:no-repeat; background-color:'+ backgroundColor+ ';background-image:url('+ backgroundImg+ '); border-radius: 10px;'" :id="'playerContainer'+playerId">
          
        <div>
          <span style="color:#c9e4e8; background:#3E8893; border:1px solid #3E8893; border-radius:50%;" class="px-2 py-2"  @click.stop="playVideo" ><v-icon color="#ffffff">mdi-play</v-icon></span>
