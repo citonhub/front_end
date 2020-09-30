b985bba919bef1d0fe21cb13220068adb70e2a38
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-09-30 15:49:27 +0100

    socket solution

--- resources/views/pages/space.blade.php
+++ resources/views/pages/space.blade.php
@@ -353,7 +353,7 @@ body {
 
    <script src="https://cdn.webrtc-experiment.com/hark.js"></script>
 
- <script src="{{ asset('js/space.js?v=4.42') }}"></script>
+ <script src="{{ asset('js/space.js?v=4.43') }}"></script>
  
      
  @endsection
