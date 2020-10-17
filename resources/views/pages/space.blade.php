6d518c9d2ef765fa6822ea325d424c5d49fd7e3e
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-10-17 01:40:27 +0100

    new commit

--- resources/views/pages/space.blade.php
+++ resources/views/pages/space.blade.php
@@ -362,7 +362,7 @@ body {
 
    <script src="https://unpkg.com/marked@0.3.6"></script>
 
- <script src="{{ asset('js/space.js?v=5.27') }}"></script>
+ <script src="{{ asset('js/space.js?v=5.28') }}"></script>
  
      
  @endsection
