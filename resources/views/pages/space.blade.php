4eef1e6c5c05512ae8f51d537df102f588ac12df
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-11-02 14:22:18 +0100

    new update

--- resources/views/pages/space.blade.php
+++ resources/views/pages/space.blade.php
@@ -374,7 +374,7 @@ body {
    <script src="https://unpkg.com/marked@0.3.6"></script>
 
 
- <script src="{{ asset('js/space.js?v=5.94') }}"></script>
+ <script src="{{ asset('js/space.js?v=5.95') }}"></script>
  
      
  @endsection
