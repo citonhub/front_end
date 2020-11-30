c2d457e6dd85b39ae60f647346e856ea04c58a03
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2020-11-30 23:46:38 +0100

    homepage header adjusted

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -6,14 +6,28 @@
  <a href="/"><img alt="citonhub logo" src="/imgs/citonhub.png" height="40px"></a>
  </div>
 
- <!-- Sign up, register button -->
- <div class="col-8 text-right pt-1">
- <a href="https://blog.citonhub.com" class="linkStyle mx-2 d-none d-md-inline-block">Blog</a>
+  <!-- Sign up, register button -->
+  <div class="col-8 text-right pt-1 d-lg-block d-none" >
+ <a href="https://blog.citonhub.com" class="linkStyle mx-3 " style="color:#000000;">Blog</a>
 
- <a href="/dashboard" class="linkStyle mx-2">Login</a>
+ <a href="/dashboard" class="linkStyle mx-3"  style="color:#000000;"> Login</a>
  
  <button class="homeButton mx-2 px-3 py-2">Sign Up</button>
 
  </div>
+
+ <div class="col-8 text-right pt-1 d-lg-none d-block pr-1" >
+
+
+ <a href="/dashboard" class="linkStyle mx-2"  style="color:#000000; font-size:13px;"> Login</a>
+ 
+ <button class="homeButton mx-2 px-2 py-2" style="font-size:13px;">Sign Up</button>
+
+ </div>
+
+
+</div>
+</div>
+<!-- ends -->
 </div>
 </div>
