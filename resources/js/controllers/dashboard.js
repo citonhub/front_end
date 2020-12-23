94a6de6b00c8fa2ff625db94f4a871ea534cddd1
Adenikym <mm5adeniky@gmail.com>
2020-12-23 10:41:18 +0100

    new

--- resources/js/controllers/dashboard.js
+++ resources/js/controllers/dashboard.js
@@ -8,7 +8,7 @@ window.io = require('socket.io-client');
 
 Vue.use(Vuex)
 
-axios.defaults.baseURL = 'https://api.beta.citonhub.com'
+axios.defaults.baseURL = 'https://api.beta.citonhub.com/api'
 
 const store = new Vuex.Store({
   state: {
