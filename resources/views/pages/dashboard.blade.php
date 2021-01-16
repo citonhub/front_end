18b5ba904d717637b1a8ab3a088c41c9d9fd93de
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-01-16 16:28:45 +0100

    another main push to beta

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -89,7 +89,7 @@
 <!-- ends -->
 
 <!-- The main compiled script for this page  -->
- <script src="{{ asset('js/dashboard.js?v=1.86') }}"></script>
+ <script src="{{ asset('js/dashboard.js?v=1.87') }}"></script>
  <!-- ends -->
      
  @endsection
