87a2f70fcc0310fcd901d220760ac61c91c1e7b0
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-01-20 14:05:42 +0100

    another push to master

--- resources/js/components/Hub/Hub.vue
+++ resources/js/components/Hub/Hub.vue
@@ -13,7 +13,7 @@
 
        <!-- hub content -->
        
-        <div class="col-lg-10 offset-lg-1 d-none d-md-block col-12 px-0 d-md-block d-none" style="background:#F5F5FB; position:absolute; height:92%; top:8%; " >
+        <div class="col-lg-10 offset-lg-1 d-none d-md-block col-12 px-0 d-md-block d-none" style="background:#F5F5FB;z-index:99999999999999; position:absolute; height:92%; top:8%; " >
 
             <div class="row px-md-4 px-4" style="background:#F5F5FB;z-index:99999999999999;">
               <!-- header -->
