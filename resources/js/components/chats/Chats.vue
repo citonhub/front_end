f00763749f9398411a6615b7b5191075bdb554fb
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-01-22 18:49:48 +0100

    another push to master

--- resources/js/components/chats/Chats.vue
+++ resources/js/components/chats/Chats.vue
@@ -195,7 +195,7 @@
      :keyField="'index_count'"
     :min-item-size="36"
     ref="messageContainer"
-       :buffer="500"
+     
     id="messageContainer"
   class="col-12 px-0 scroller" 
 
@@ -724,7 +724,7 @@
      :keyField="'index_count'"
     :min-item-size="36"
     ref="messageContainersmall"
-      :buffer="500"
+     
     id="messageContainersmall"
   class="col-12 scroller px-0" 
 
