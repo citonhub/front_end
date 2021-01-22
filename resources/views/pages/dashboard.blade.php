d485d35bd9ada92f9efc03948732c019aa567545
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-01-22 19:06:42 +0100

    another push to master

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -92,7 +92,7 @@
 <!-- ends -->
 
 <!-- The main compiled script for this page  -->
- <script src="{{ asset('js/dashboard.js?v=3.18') }}"></script>
+ <script src="{{ asset('js/dashboard.js?v=3.19') }}"></script>
  <!-- ends -->
      
  @endsection
