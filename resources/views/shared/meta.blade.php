fb76de6ae3dc51c58d2b8cff3947617de9279557
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-01-22 15:48:38 +0100

    another push to master

--- resources/views/shared/meta.blade.php
+++ resources/views/shared/meta.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="shortcut icon" href="/logo.ico" type="image/x-icon">
 <link rel="icon" href="/logo.ico" type="image/x-icon"> 
