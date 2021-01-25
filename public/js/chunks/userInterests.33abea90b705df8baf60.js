56ff6049286f044288f3702b7fe86e801793be95
Adenikym <mm5adeniky@gmail.com>
2021-01-25 18:12:38 +0100

    new

---
+++ public/js/chunks/userInterests.33abea90b705df8baf60.js
@@ -0,0 +1,179 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["userInterests"],{
+
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/auth/Interests.vue?vue&type=script&lang=js&":
+/*!*************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/auth/Interests.vue?vue&type=script&lang=js& ***!
+  \*************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\projects\\for git\\new commit\\New folder\\front_end\\resources\\js\\components\\auth\\Interests.vue: Unexpected token, expected \"{\" (45:2)\n\n\u001b[0m \u001b[90m 43 | \u001b[39m  methods\u001b[33m:\u001b[39m{\u001b[0m\n\u001b[0m \u001b[90m 44 | \u001b[39m      displayDiaries()\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 45 | \u001b[39m  }\u001b[0m\n\u001b[0m \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 46 | \u001b[39m}\u001b[0m\n\u001b[0m \u001b[90m 47 | \u001b[39m\u001b[0m\n    at Parser._raise (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:742:17)\n    at Parser.raiseWithData (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:735:17)\n    at Parser.raise (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:729:17)\n    at Parser.unexpected (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:8757:16)\n    at Parser.expect (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:8743:28)\n    at Parser.parseBlock (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:11620:10)\n    at Parser.parseFunctionBody (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10634:24)\n    at Parser.parseFunctionBodyAndFinish (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10617:10)\n    at Parser.parseMethod (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10579:10)\n    at Parser.parseObjectMethod (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10495:19)\n    at Parser.parseObjPropValue (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10537:23)\n    at Parser.parseObjectMember (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10461:10)\n    at Parser.parseObj (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10374:25)\n    at Parser.parseExprAtom (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9995:28)\n    at Parser.parseExprSubscripts (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9602:23)\n    at Parser.parseMaybeUnary (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9582:21)\n    at Parser.parseExprOps (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9452:23)\n    at Parser.parseMaybeConditional (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9425:23)\n    at Parser.parseMaybeAssign (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9380:21)\n    at Parser.parseObjectProperty (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10512:101)\n    at Parser.parseObjPropValue (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10537:101)\n    at Parser.parseObjectMember (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10461:10)\n    at Parser.parseObj (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:10374:25)\n    at Parser.parseExprAtom (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9995:28)\n    at Parser.parseExprSubscripts (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9602:23)\n    at Parser.parseMaybeUnary (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9582:21)\n    at Parser.parseExprOps (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9452:23)\n    at Parser.parseMaybeConditional (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9425:23)\n    at Parser.parseMaybeAssign (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:9380:21)\n    at Parser.parseExportDefaultExpression (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:12283:24)\n    at Parser.parseExport (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:12185:31)\n    at Parser.parseStatementContent (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:11185:27)\n    at Parser.parseStatement (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:11081:17)\n    at Parser.parseBlockOrModuleBlockBody (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:11656:25)\n    at Parser.parseBlockBody (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:11642:10)\n    at Parser.parseTopLevel (C:\\projects\\for git\\new commit\\New folder\\front_end\\node_modules\\@babel\\parser\\lib\\index.js:11012:10)");
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/auth/Interests.vue?vue&type=template&id=059ce666&":
+/*!*****************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/auth/Interests.vue?vue&type=template&id=059ce666& ***!
+  \*****************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", [
+    _c(
+      "div",
+      { staticClass: "col-lg-12 d-sm-none" },
+      [
+        _c("h1", { staticClass: "text-center" }, [
+          _vm._v(" Select Your Interests")
+        ]),
+        _vm._v(" "),
+        _c("p", { staticClass: "text-center" }, [_vm._v("Pick one or more.")]),
+        _vm._v(" "),
+        _vm._l(_vm.Interests, function(interest, index) {
+          return _c(
+            "div",
+            {
+              key: index,
+              staticClass: "interest col-lg-12 d-flex mb-2",
+              staticStyle: {
+                "background-color": "rgba( 125,179,229,0.1)",
+                "font-size": "1.6rem",
+                "font-weight": "bold"
+              }
+            },
+            [
+              _c("div", { staticClass: "form-check" }, [
+                _c("input", {
+                  staticClass: "form-check-input mt-2",
+                  attrs: { type: "checkbox", value: "", id: "defaultCheck1" }
+                }),
+                _vm._v(" "),
+                _c(
+                  "label",
+                  {
+                    staticClass: "form-check-label",
+                    attrs: { for: "defaultCheck1" }
+                  },
+                  [_vm._v("\r\n   " + _vm._s(interest) + "\r\n  ")]
+                )
+              ])
+            ]
+          )
+        }),
+        _vm._v(" "),
+        _c(
+          "div",
+          { staticClass: "buttons mt-2 col-5 offset-7 d-flex" },
+          [
+            _c(
+              "p",
+              { staticStyle: { "font-size": "1.2rem", "font-weight": "bold" } },
+              [_vm._v("Skip")]
+            ),
+            _vm._v(" "),
+            _c(
+              "v-btn",
+              {
+                staticClass: "ml-2 px-2",
+                staticStyle: {
+                  color: "white",
+                  "background-color": "#3C87CD",
+                  "border-radius": "10px",
+                  height: "30px"
+                }
+              },
+              [_vm._v("Done")]
+            )
+          ],
+          1
+        )
+      ],
+      2
+    )
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
+
+/***/ }),
+
+/***/ "./resources/js/components/auth/Interests.vue":
+/*!****************************************************!*\
+  !*** ./resources/js/components/auth/Interests.vue ***!
+  \****************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Interests_vue_vue_type_template_id_059ce666___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Interests.vue?vue&type=template&id=059ce666& */ "./resources/js/components/auth/Interests.vue?vue&type=template&id=059ce666&");
+/* harmony import */ var _Interests_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Interests.vue?vue&type=script&lang=js& */ "./resources/js/components/auth/Interests.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Interests_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _Interests_vue_vue_type_template_id_059ce666___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _Interests_vue_vue_type_template_id_059ce666___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/auth/Interests.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/auth/Interests.vue?vue&type=script&lang=js&":
+/*!*****************************************************************************!*\
+  !*** ./resources/js/components/auth/Interests.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Interests_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib??ref--4-0!../../../../node_modules/vue-loader/lib??vue-loader-options!./Interests.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/auth/Interests.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Interests_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/auth/Interests.vue?vue&type=template&id=059ce666&":
+/*!***********************************************************************************!*\
+  !*** ./resources/js/components/auth/Interests.vue?vue&type=template&id=059ce666& ***!
+  \***********************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Interests_vue_vue_type_template_id_059ce666___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./Interests.vue?vue&type=template&id=059ce666& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/auth/Interests.vue?vue&type=template&id=059ce666&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Interests_vue_vue_type_template_id_059ce666___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Interests_vue_vue_type_template_id_059ce666___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ })
+
+}]);
--- resources/js/components/auth/Interests.vue
+++ resources/js/components/auth/Interests.vue
@@ -2,7 +2,7 @@
 
 <div>
     <div class="col-lg-12 d-sm-none">
-<h1 class="text-center" > Select Your Interests-sm</h1>
+<h1 class="text-center" > Select Your Interests</h1>
 <p class="text-center">Pick one or more.</p>
 <div v-for="(interest,index) in Interests" :key="index" class="interest col-lg-12 d-flex mb-2" style="background-color:rgba( 125,179,229,0.1); font-size:1.6rem;font-weight:bold;
 ">
@@ -15,6 +15,10 @@
     
 
   
+</div>
+
+<div  class="buttons mt-2 col-5 offset-7 d-flex">
+  <p style="font-size:1.2rem;font-weight:bold;">Skip</p>  <v-btn @click="displayDiaries()"    class="ml-2 px-2" style="color:white;background-color: #3C87CD;border-radius:10px;height:30px;">Done</v-btn>
 </div>
     </div>
 
@@ -35,5 +39,13 @@
        Interests: ['Javascript', 'PHP', 'Python', 'React', 'CSS', 'HTML', 'Flutter', 'Golang', 'Java', 'C++', 'Dart'],
       }
     },
+
+    methods:{
+        displayDiaries(){
+            this.$router.push({
+                path:'/follow-diaries'
+            })
+        }
+    }
   }
 </script>
