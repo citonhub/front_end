452370c9c261716a67a742f1385b5ab7d416fa3b
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-01-28 13:26:27 +0100

    Revert "part of chat cleaned up"
    This reverts commit 539e8faa53281bf033386a23afa4ef76180cc982.

--- resources/js/controllers/dashboard.js
+++ resources/js/controllers/dashboard.js
@@ -10,7 +10,10 @@ Vue.use(Vuex)
 
 //axios.defaults.baseURL = 'http://localhost:8000/api'
 //axios.defaults.baseURL = 'http://api.citonhubnew.com/api'
-axios.defaults.baseURL = 'https://api.citonhub.com/api'
+//axios.defaults.baseURL = 'https://api.citonhub.com/api'
+
+axios.defaults.baseURL = 'https://api.beta.citonhub.com/api'
+
 const store = new Vuex.Store({
   state: {
     user: null
@@ -1796,6 +1799,7 @@ const app = new Vue({
      fromProfileUsername:'',
      codeFromChat:false,
      componentIsLoading:false,
+     showCodeboxBtn:true,
      },
      mounted: function () {
       window.thisUserState = this;
@@ -2016,28 +2020,26 @@ const app = new Vue({
         },
         saveDeviceInfo:function(deviceId){
   
+          axios.post('/save-user-device',{
+            deviceId: deviceId,
+            device_name: navigator.appName,
+            device_platform: navigator.platform
+              })
+      .then(response => {
+        
+       if (response.status == 200) {
+          
     
-
-      axios.post('/user-post-data',{
-        deviceId: deviceId,
-        device_name: navigator.appName,
-        device_platform: navigator.platform
-          })
-  .then(response => {
-    
-   if (response.status == 200) {
-      
-   
-    
-    }else{
-      console.log(response.status);
-    }
-    
-    
-  })
-  .catch(error => {
-    console.log(error);
-  })
+        
+        }else{
+          console.log(response.status);
+        }
+        
+        
+      })
+      .catch(error => {
+        console.log(error);
+      })
   
         },
         markSpaceRead:function(spaceId){
@@ -2153,7 +2155,8 @@ const app = new Vue({
       'connections': userProfile.connections,
       'background_color': userProfile.background_color,
       'unread': userProfile.unread,
-      'points': userProfile.points
+      'points': userProfile.points,
+      'user_onboarded':userProfile.user_onboarded,
       };
         
 
@@ -2244,7 +2247,7 @@ const app = new Vue({
             this.handleSpaceData([messageData])
             
 
-          this.$root.sortChatList();
+        
 
             this.scrollToBottom();
 
@@ -2514,10 +2517,12 @@ const app = new Vue({
       })
       .listenForWhisper('typing', (e) => {
 
-
+            
         this.$root.typinguser = e.user;
          this.$root.typing = e.typing;
          this.$root.typingSpace = e.spaceId;
+
+          this.stopTying();
 
 
 
@@ -2862,6 +2867,13 @@ const app = new Vue({
   
        
      },
+     stopTying:_.debounce( function () {
+
+      this.$root.typinguser = '';
+     this.$root.typing = false;
+     this.$root.typingSpace = '';
+
+      }, 1000),
   checkIfMessageExist(data){
 
     let messageData = this.$root.Messages.filter((message)=>{
@@ -2950,6 +2962,39 @@ const app = new Vue({
       if(actionName == 'sort_chat'){
 
         this.$root.sortChatList();
+
+      }
+
+      if(actionName == 'message_engine'){
+
+         this.$root.updateSpaceTracker(extraData.space_id,extraData);
+          
+            this.$root.clearUnreadMessageRemote(extraData.message_id);
+         
+
+             // update unread messages into local storage
+
+      let unreadStoredMsg = this.$root.getLocalStore('unread_messages_' + extraData.space_id + this.$root.username);
+
+   unreadStoredMsg.then((result)=>{
+
+     let finalResultUnread = JSON.parse(result);
+
+      finalResultUnread.push(extraData)
+
+
+      localforage.setItem('unread_messages_' + extraData.space_id + this.$root.username,JSON.stringify(finalResultUnread)).then( ()=> {
+        
+
+       }).then(function (value) {
+       // we got our value
+
+       }).catch(function (err) {
+      console.log(err)
+      // we got an error   
+       });
+
+      });
 
       }
    
@@ -3057,36 +3102,9 @@ handleSpaceData: function(returnData){
           MessagesFull.messages.push(message);
 
           // update into local storage
-            this.$root.LocalStore('full_' + space.space_id  + this.$root.username,MessagesFull);
+            this.$root.LocalStore('full_' + space.space_id  + this.$root.username,MessagesFull,false,'message_engine',message);
 
-            this.$root.updateSpaceTracker(space.space_id,message);
-          
-            this.$root.clearUnreadMessageRemote(message.message_id);
-         
-
-             // update unread messages into local storage
-
-      let unreadStoredMsg = this.$root.getLocalStore('unread_messages_' + space.space_id + this.$root.username);
-
-   unreadStoredMsg.then((result)=>{
-
-     let finalResultUnread = JSON.parse(result);
-
-      finalResultUnread.push(message)
-
-
-      localforage.setItem('unread_messages_' + space.space_id + this.$root.username,JSON.stringify(finalResultUnread)).then( ()=> {
-        
-
-       }).then(function (value) {
-       // we got our value
-
-       }).catch(function (err) {
-      console.log(err)
-      // we got an error   
-       });
-
-      });
+           
 
         });
 
@@ -3214,6 +3232,8 @@ handleSpaceData: function(returnData){
 
     });
 
+    this.$root.sortChatList();
+
     // save into local storage
 
     let storedChat = this.$root.getLocalStore('user_chat_list'+ this.$root.username);
@@ -3255,9 +3275,9 @@ handleSpaceData: function(returnData){
      
              });
 
-    this.$root.LocalStore('user_chat_list' + this.$root.username,finalResult,false,'sort_chat');
+    this.$root.LocalStore('user_chat_list' + this.$root.username,finalResult);
 
-   
+     this.$root.sortChatList();
     
     
 
@@ -4341,7 +4361,7 @@ if (mediaElement) {
               
             if(master){
               _this.openAudioRoom();
-              _this.dataconnection.open('data' + _this.$root.selectedSpace.space_id)
+              _this.dataconnection.openOrJoin('data' + _this.$root.selectedSpace.space_id)
             }else{
 
               _this.roomNotExist = true;
@@ -4440,7 +4460,7 @@ this.$root.dataconnection = undefined;
     
     let _this = this;
   
-    this.$root.audioconnection.open('audio' + this.$root.selectedSpace.space_id, () =>{
+    this.$root.audioconnection.openOrJoin('audio' + this.$root.selectedSpace.space_id, () =>{
 
         
       _this.$root.connectingToSocket = false;
@@ -4510,7 +4530,7 @@ this.$root.dataconnection = undefined;
            OfferToReceiveVideo: true
        };
       
-     this.$root.connection.open('screen' + this.$root.selectedSpace.space_id, function() {
+     this.$root.connection.openOrJoin('screen' + this.$root.selectedSpace.space_id, function() {
       
     });
 
