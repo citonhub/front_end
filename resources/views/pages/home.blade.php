36b4140a23cea7b83cdd7dcaccd17b2863a156f1
Adenikym <mm5adeniky@gmail.com>
2021-03-03 21:03:28 +0100

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -146,10 +146,17 @@
 
 </div>
 
-<div class="wallet-option d-lg-none d-md-none d-block col-12 px-0" style="width:100%;height:400px;margin-top:150px;">
+<div class="wallet-option d-lg-none d-md-none d-sm-block d-none col-12 px-0" style="width:100%;height:400px;margin-top:150px;">
 <img src="imgs/topbg.png" alt="" width="100%" height="100%">
 <img src="imgs/credit-vector.png"  alt="" style="position:absolute;z-index:1;left:22%;top:-150px;width:450px;height:330px;">
-<p class="text-center"  style="position:absolute;z-index:1;left:33%;top:200px;color:black;">Grow developer communities and earn <br> from paid sessions and paid communities.</p>
+<p class="text-center"  style="position:absolute;z-index:1;left:33%;top:200px;color:white;">Grow developer communities and earn <br> from paid sessions and paid communities.</p>
+
+</div>
+
+<div class="wallet-option d-lg-none d-md-none d-sm-none d-block col-12 px-0" style="width:100%;height:450px;margin-top:130px;">
+<img src="imgs/topbg.png" alt="" width="100%" height="100%">
+<img src="imgs/credit-vector.png"  alt="" style="position:absolute;z-index:1;left:-5%;top:-90px;width:450px;height:330px;">
+<p class="text-center"  style="position:absolute;z-index:1;left:13%;top:260px;color:white;">Grow developer communities and earn <br> from paid sessions and paid communities.</p>
 
 </div>
 
@@ -170,8 +177,14 @@
 </div>
 
 
-<div class="collab col-12 d-lg-none d-md-none d-sm-block  px-0 py-0" style="">
+<div class="collab col-12 d-lg-none d-md-none d-sm-block d-none  px-0 py-0" style="">
 <img src="imgs/collab.png" class="offset-3" style="width:395px;height:198px;" alt="">
+
+<p class="text-center mt-4" style="font-weight:bold;font-size:1.2rem;">Collaborate with other developers on <br> a variety of projects</p>
+</div>
+
+<div class="collab col-12 d-lg-none d-md-none d-sm-none d-block  px-0 py-0" style="">
+<img src="imgs/collab.png" class="" style="width:395px;height:198px;" alt="">
 
 <p class="text-center mt-4" style="font-weight:bold;font-size:1.2rem;">Collaborate with other developers on <br> a variety of projects</p>
 </div>
@@ -191,20 +204,25 @@
 <p class="text-center"  style="position:absolute;z-index:1;left:38%;top:350px;color:white;font-size:1.2rem;">Teach and mentor upcoming developers </p>
 </div>
 
-<div class="teach d-lg-none d-md-none d-block col-12 px-0" style="width:100%;height:400px;margin-top:150px;">
+<div class="teach d-lg-none d-md-none d-sm-block d-none col-12 px-0" style="width:100%;height:400px;margin-top:150px;">
 <img src="imgs/topbg.png" alt="" width="100%" height="100%">
 
 <p class="text-center"  style="position:absolute;z-index:1;left:38%;top:240px;color:white;font-size:1.2rem;">Teach and mentor upcoming developers </p>
 </div>
 
 
+<div class="teach d-lg-none d-md-none d-sm-none d-block col-12 px-0" style="width:100%;height:450px;margin-top:150px;">
+<img src="imgs/topbg.png" alt="" width="100%" height="100%">
+
+<p class="text-center"  style="position:absolute;z-index:1;left:5%;top:240px;color:white;font-size:1.2rem;">Teach and mentor upcoming developers </p>
+</div>
 <!--ends-->
 
 <!--features section-->
 
-<div class="features col-lg-12 px-0 d-md-block d-none">
+<div class="features col-lg-12 px-0 d-lg-block  d-none">
 
-<h2 style="font-weight:bold;" class="text-center">Features</h2>
+<h2 style="font-weight:bold;" class="text-center" style="color:white;font-size:1rem;">Features</h2>
 
 <div  class="all-features mt-4 col-lg-8 offset-lg-2 d-flex ">
 
@@ -259,9 +277,9 @@
 <!--smaller screens-->
 
 
-<div class="features  d-md-none d-block px-0">
+<div class="features d-lg-none  d-md-none d-sm-block d-none px-0">
 
-<h2 style="font-weight:bold;" class="text-center">Features</h2>
+<h2 style="font-weight:bold;" class="text-center" style="color:white;">Features</h2>
 
 <div  class="all-features mt-4 col-10 offset-1 d-flex ">
 
@@ -316,6 +334,62 @@
 </div>
 </div>
 
+<div class="features d-lg-none  d-md-none d-sm-none d-block px-0">
+
+<h2 style="font-weight:bold;" class="text-center" style="color:white;">Features</h2>
+
+<div  class="all-features mt-4 col-12 d-flex ">
+
+<div class="col-4 ">
+<div class="box shadow" style="height:170px;width:150px;">
+<img src="/imgs/phone-vector.png" alt=""></div>
+<p style="font-weight:bold;margin-left:5%;" class="mt-2">Voice Chat</p>
+</div>
+
+<div class="col-4" style="margin-left:18%;">
+<div class="box " style="height:170px;width:150px;margin-left:10%;">
+<img src="/imgs/code-vector.png" alt="">
+</div>
+<p style="font-weight:bold;margin-left:7%;" class="mt-2">Screen sharing</p>
+</div>
+</div>
+
+<div  class="all-features mt-4 col-12 d-flex ">
+
+<div class="col-4 ">
+<div class="box" style="height:170px;width:150px;">
+<img src="/imgs/live-vector.png" alt="">
+
+</div>
+<p style="font-weight:bold;margin-left:5%;" class="mt-2">Live Coding</p>
+</div>
+
+
+
+
+<div class="col-4 " style="margin-left:20%;">
+<div class="box" style="height:170px;width:150px;">
+<img src="/imgs/collab-vector.png" alt=""></div>
+
+<p style="font-weight:bold;" class="mt-2">Collaborate on projects.</p>
+
+</div>
+</div>
+
+<div  class="all-features mt-4 col-12 d-flex ">
+<div class="col-4 ">
+<div class="box " style="height:170px;width:150px;"></div>
+
+<p style="font-weight:bold;margin-left:5%;" class="mt-2">Projects showcase.</p>
+</div>
+
+<div class="col-4 " style="margin-left:20%;">
+<div class="box" style="height:170px;width:150px;">
+<img src="/imgs/document-vector.png" alt=""></div>
+<p style="font-weight:bold;margin-right:31%;" class="mt-2 text-center">Document your learning <br> process in diaries</p>
+</div>
+</div>
+</div>
 <!--ends-->
 
 <!--footer begins-->
@@ -326,9 +400,11 @@
 
 <p class="ml-4" style="margin-top:220px;" >&copy; CitonHub 2021</p>
 
-<img src="imgs/twitter-bird.png" style="height: 17px;width: 21.25px;margin-top:220px;" class="offset-5" alt="">
-<img src="imgs/instagram-home.png"  style="height: 17px;width:17px;margin-top:220px;" class="ml-2" alt="">
+<img src="imgs/twitter-bird.png" style="height: 17px;width: 21.25px;margin-top:220px;" class="offset-5 d-lg-block d-none" alt="">
+<img src="imgs/instagram-home.png"  style="height: 17px;width:17px;margin-top:220px;" class="ml-2 d-lg-block d-none" alt="">
 
+<img src="imgs/twitter-bird.png" style="height: 17px;width: 21.25px;margin-top:220px;" class="ml-4 d-lg-none d-block " alt="">
+<img src="imgs/instagram-home.png"  style="height: 17px;width:17px;margin-top:220px;" class="ml-2 d-lg-none d-block" alt="">
 
 <ul style="list-style:none;color:white;margin-top:220px;" class="d-lg-flex d-none">
 <li>Contact Us</li>
@@ -336,7 +412,7 @@
 <li class="ml-4">Privacy Policy</li>
 </ul>
 
-<ul style="list-style:none;color:white;margin-top:160px;" class="d-lg-none d-block">
+<ul style="list-style:none;color:white;margin-top:140px;" class="d-lg-none d-block">
 <li class="ml-2">Contact Us</li>
 <li class="ml-2 mt-1">Terms of Use</li>
 <li class="ml-2 mt-1">Privacy Policy</li>
