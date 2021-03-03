2301f276be3f709f8406d00e9f9272dc5f4fec00
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-03-03 14:09:42 +0100

    wallet pages

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -90,7 +90,7 @@
 <!-- ends -->
 
 <!-- The main compiled script for this page  -->
- <script src="{{ asset('js/dashboard.js?v=5.01') }}"></script>
+ <script src="{{ asset('js/dashboard.js?v=5.02') }}"></script>
  <!-- ends -->
      
  @endsection
