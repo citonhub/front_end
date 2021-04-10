22e4eeaae2e23648091ca7791c2e27d07510796c
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-04-10 12:04:07 +0100

    new changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -150,6 +150,11 @@
        
       </div>
 
+      <div class="col-12 pt-3 mt-5 text-center">
+
+     <a href="https://www.producthunt.com/posts/citonhub?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-citonhub" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=291655&theme=light" alt="Citonhub - All-in-one Slack, Meet, Codepen & Patreon platform for devs | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+    </div>
+
       <div class="col-12 pt-2 mt-5 text-center">
      <img src="imgs/all_in_one_full.svg" alt="" style="z-index:1;width:100%; height:55px;"> 
      </div>
