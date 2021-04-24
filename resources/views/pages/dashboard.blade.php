bca92f87d7f5d4c101417d5e426f037bbbb52a71
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-04-24 16:26:04 +0100

    new changes

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -92,7 +92,7 @@
 <!-- ends -->
 
 <!-- The main compiled script for this page  -->
- <script src="{{ asset('js/dashboard.js?v=7.03') }}"></script>
+ <script src="{{ asset('js/dashboard.js?v=7.04') }}"></script>
  <!-- ends -->
      
  @endsection
