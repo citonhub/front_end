fbfe088ce4d21021dd74fc1e30c79bb12c7cf150
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2021-05-06 13:07:40 +0100

    new changes

--- resources/js/components/chats/resources.vue
+++ resources/js/components/chats/resources.vue
@@ -308,8 +308,9 @@ methods:{
              window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
 
              this.$root.chatComponent.innerSideBarContent = '';
-            
-            this.$root.chatComponent.innerSideBarContent = '';
+
+             this.$root.chatInnerSideBar = false;
+           
        
         },
 
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -98,7 +98,7 @@
 <!-- ends -->
 
 <!-- The main compiled script for this page  -->
- <script src="{{ asset('js/dashboard.js?v=7.87') }}"></script>
+ <script src="{{ asset('js/dashboard.js?v=7.88') }}"></script>
  <!-- ends -->
      
  @endsection
